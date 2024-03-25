1c53f721b7a6a6c1a3afdbd35f999eefcee912ee
Wang Cheng <99178447+bowjacon@users.noreply.github.com>
2024-03-25 20:32:48 +0800

    My_Project

--- Core/Inc/my_config.h
+++ Core/Inc/my_config.h
@@ -3,7 +3,7 @@
 
 #define  Long_Distance_Interval   50 //测距时间间隔为50ms
 #define  Short_Distance_Interval  10 //测距时间间隔为10ms
-#define Process_Count 100 //处理采样总数
-#define Short_Count 20 //短距离
+#define Process_Count 50 //处理采样总数
+#define Short_Count 10 //短距离
 
 #endif //EXAMPLE_MY_CONFIG_H
