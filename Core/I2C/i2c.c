5c8c6625255d293b1246d16ae88e04c2ed70bf24
bowjacon <2772408947@qq.com>
2024-04-24 19:45:49 +0800

    NEW

--- Core/I2C/i2c.c
+++ Core/I2C/i2c.c
@@ -2,7 +2,7 @@
  * @Author: bowjacon 2772408947@qq.com
  * @Date: 2024-04-21 20:22:42
  * @LastEditors: bowjacon 2772408947@qq.com
- * @LastEditTime: 2024-04-24 12:06:41
+ * @LastEditTime: 2024-04-24 19:43:39
  * @FilePath: /74HC595/Core/I2C/i2c.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -26,129 +26,107 @@
 #include <stdint.h>
 #include <string.h>
 
-// /*
-//  * I2C初始化
-//  */
-// void M_I2C_Init(void) {
-//     GPIO_InitTypeDef GPIO_InitStructure;
-//     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-//     GPIO_InitStructure.GPIO_Pin = I2C_SCL_Pin | I2C_SDA_Pin;
-//     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-//     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-//     GPIO_Init(I2C_PORT, &GPIO_InitStructure);
-// }
-/*
+/**
+ * @brief
  * 开始信号
  */
-static void M_I2C_Start(void) {
+void M_I2C_Start(void) {
     I2C_SDA(1);
     I2C_SCL(1);
-    Delay_us(SCL_DELAY);
     I2C_SDA(0);
 }
 
-/*
- * 结束信号
+/**
+ * @brief 停止信号
  */
-static void M_I2C_Stop(void) {
+
+void M_I2C_Stop(void) {
     I2C_SDA(0);
     I2C_SCL(1);
-    Delay_us(SCL_DELAY);
     I2C_SDA(1);
-
 }
-/*
- * 应答信号：SCL最低需要保持0.6us
+/**
+ * @brief 脉冲信号
+ *
  */
-static void M_I2C_Ack(void) {
+void M_I2C_Clock(void) {
+    I2C_SCL(0);
     I2C_SCL(1);
-    Delay_us(SCL_DELAY);
     I2C_SCL(0);
 }
-/*
- * 非应答信号，由主机发送
- */
-static void M_I2C_NAck(void) {
-    I2C_SDA(1);
-    I2C_SCL(1);
-    Delay_us(SCL_DELAY);
-    I2C_SCL(0);
-}
-/*
+/**
  * 发送一个字节数据
  */
 void M_I2C_Transmit_Byte(uint8_t byte) {
+    I2C_SCL(0);
     for (uint8_t j = 0; j < 8; j++) {
-        I2C_SDA((byte & (0x80 >> j)) ? 1 : 0);
-        Delay_us(SCL_DELAY);
+        I2C_SDA((byte & (0x80 >> j)));
         I2C_SCL(1);
-        Delay_us(SCL_DELAY);
         I2C_SCL(0);
-        Delay_us(SCL_DELAY);
-        //释放总线
-        if (j == 7) {
-            I2C_SDA(1);
-        }
     }
-    M_I2C_Ack();
 }
 
-/*
+/**
  * 发送n个数据(中间层)
  */
 void M_I2C_Transmit(const uint8_t *data, uint8_t n) {
     for (uint8_t i = 0; i < n; i++) {
         M_I2C_Transmit_Byte(data[i]);
+        M_I2C_Clock();
     }
 }
 
-/*
+/**
  * 接收一个字节数据
  */
 void M_I2C_Receive_Byte(uint8_t *data) {
     uint8_t byte = 0;
+    I2C_SCL(0);
+    I2C_SDA(1);
     for (uint8_t j = 0; j < 8; j++) {
         I2C_SCL(1);
-        Delay_us(SCL_DELAY);
-        byte |= Read_SDA() ? (0x80 >> j) : 0;
+        if (Read_SDA() == 1) {
+            byte |= (0x80 >> j);
+        }
         I2C_SCL(0);
-        Delay_us(SCL_DELAY);
     }
     *data = byte;
 }
 
-/*
+/**
  * 接受n个数据(中间层)
  */
 void M_I2C_Reicive(uint8_t reg_adress, uint8_t *data, uint8_t n) {
     for (uint8_t i = 0; i < n; i++) {
         M_I2C_Receive_Byte(&data[i]);
-        if (i != n - 1) {
-            M_I2C_Ack();
-        }
+        M_I2C_Clock();
+        reg_adress++;
     }
-    M_I2C_NAck();
 }
-/*
+/**
  * 写入指定个数数据
  */
 void M_I2C_Transmit_Data(uint8_t reg_adress, const uint8_t *data, uint8_t n) {
-    M_I2C_Start();
     uint8_t length = 2 + n;
     uint8_t send_packet[length];
-    uint8_t adress[2] = {MPU6050_ADDRESS | Write_Mode, reg_adress};
+    uint8_t adress[2] = {MPU6050_ADDRESS, reg_adress};
+    M_I2C_Start();
     Connect_Array(send_packet, adress, 2, data, n);
     M_I2C_Transmit(send_packet, length);
     M_I2C_Stop();
 }
 
-/*
+/**
  * 读取指定个数数据
  */
 void M_I2C_ReiciveByte_Data(uint8_t reg_adress, uint8_t *data, uint8_t n) {
     M_I2C_Start();
-    uint8_t adress[2] = {MPU6050_ADDRESS | Read_Mode, reg_adress};
+    uint8_t adress[2] = {MPU6050_ADDRESS, reg_adress};
     M_I2C_Transmit(adress, 2);
+    M_I2C_Start();
+    uint8_t send_data[] = {MPU6050_ADDRESS | Read_Mode};
+    M_I2C_Transmit(send_data, 1);
     M_I2C_Reicive(reg_adress, data, n);
+    M_I2C_Clock();
     M_I2C_Stop();
 }
--- Core/I2C/i2c.h
+++ Core/I2C/i2c.h
@@ -2,7 +2,7 @@
  * @Author: bowjacon 2772408947@qq.com
  * @Date: 2024-04-21 20:22:48
  * @LastEditors: bowjacon 2772408947@qq.com
- * @LastEditTime: 2024-04-23 22:42:15
+ * @LastEditTime: 2024-04-24 19:44:10
  * @FilePath: /74HC595/Core/I2C/i2c.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -40,24 +40,9 @@
 #define SCL_DELAY 4
 #define Write_Mode 0
 #define Read_Mode 1
-typedef enum {
-    ACK = 0,
-    NACK = 1,
-} ACK_FLAG;
-#define I2C_SCL(x)                                                             \
-    do {                                                                       \
-        if (x)                                                                 \
-            GPIO_SetBits(I2C_PORT, I2C_SCL_Pin);                               \
-        else                                                                   \
-            GPIO_ResetBits(I2C_PORT, I2C_SCL_Pin);                             \
-    } while (0)
-#define I2C_SDA(x)                                                             \
-    do {                                                                       \
-        if (x)                                                                 \
-            GPIO_SetBits(I2C_PORT, I2C_SDA_Pin);                               \
-        else                                                                   \
-            GPIO_ResetBits(I2C_PORT, I2C_SDA_Pin);                             \
-    } while (0)
+
+#define I2C_SCL(x) GPIO_WriteBit(I2C_PORT, I2C_SCL_Pin, x)
+#define I2C_SDA(x) GPIO_WriteBit(I2C_PORT, I2C_SDA_Pin, x)
 
 void M_I2C_Init(void);
 void M_I2C_Transmit(const uint8_t *data, uint8_t n);
@@ -68,24 +53,9 @@ void M_I2C_Transmit(const uint8_t *data, uint8_t n);
 #define Read_SDA() GPIO_ReadInputDataBit(I2C_PORT, I2C_SDA_Pin)
 
 /*
- * 改变SDA为输出模式
- */
-#define IN_SDA()                                                               \
-    GPIO_Init(I2C_PORT, &((GPIO_InitTypeDef){.GPIO_Pin = I2C_SDA_Pin,          \
-                                             .GPIO_Mode = GPIO_Mode_IPU,       \
-                                             .GPIO_Speed = GPIO_Speed_50MHz}))
-
-/*
- * 改变SDA为输入模式
- */
-#define OUT_SDA()                                                              \
-    GPIO_Init(I2C_PORT, &((GPIO_InitTypeDef){.GPIO_Pin = I2C_SDA_Pin,          \
-                                             .GPIO_Mode = GPIO_Mode_Out_PP,    \
-                                             .GPIO_Speed = GPIO_Speed_50MHz}))
-/*
  * 拼接数组
  */
-#define Connect_Array(dest, src1, len1, src2, len2)                       \
+#define Connect_Array(dest, src1, len1, src2, len2)                            \
     do {                                                                       \
         for (int i = 0; i < (len1); i++) {                                     \
             (dest)[i] = (src1)[i];                                             \
